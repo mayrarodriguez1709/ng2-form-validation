8a59b2817cca452883e9bd607a16164f04bd6cb0
Mayra A. Rodriguez <mayrarodriguez1709@gmail.com>
2017-01-26 12:22:22 -0500

    Validate required

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 
 
 @Component({
@@ -8,5 +8,44 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  loginForm: FormGroup;
+
+  name: AbstractControl;
+  email: AbstractControl;
+  password: AbstractControl;
+
+  constructor(
+    private fb: FormBuilder
+  ){
+    this.loginForm = this.buildLoginForm;
+    this.createControls();
+    
+  }
+
+  private get buildLoginForm(){
+    let inputs = {
+      'name' : [ '', Validators.required],
+      'email' : [ '', Validators.required],
+      'password' : [ '', Validators.required]
+    };
+
+    return this.fb.group(inputs)
+  }
+
+  createControls(){
+    this.name = this.loginForm.controls['name'];
+    this.email = this.loginForm.controls['email'];
+    this.password = this.loginForm.controls['password'];
+  }
   
+
+  onSubmit(){
+    let name = this.loginForm.get('name').value;
+    let email = this.loginForm.get('email').value;
+    let password = this.loginForm.get('password').value;
+
+    console.log('¡Formulario Validado!')
+    console.log(this.loginForm.value)
+  }
 }
