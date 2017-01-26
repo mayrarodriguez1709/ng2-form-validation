95b89ef4b2ed05478c6494c07afd39f10ae9d089
Mayra A. Rodriguez <mayrarodriguez1709@gmail.com>
2017-01-26 11:45:46 -0500

    bootstrap sass added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+  
 }
